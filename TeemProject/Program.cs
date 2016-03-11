6242c515c4b7ff16957d850f497eb3182260ad36
linaZaytseva <elinazaytseva@rambler.ru>
2016-03-11 19:33:06 +0300

    test

--- TeemProject/Program.cs
+++ TeemProject/Program.cs
@@ -17,6 +17,7 @@ namespace TeemProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            int a = 10;
         }
     }
 }

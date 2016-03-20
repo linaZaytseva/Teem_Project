5c265e29cad3a4a1caae6b8f2da40828b7efa055
Владислав <vlad2996@outlook.com>
2016-03-20 18:46:52 +0300

    Добавлен новый класс данных

---
+++ TeemProject/Database/MealCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeemProject
+{
+    public class MealCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public MealCategory(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
